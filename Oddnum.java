f4b035a741cef79df7ab1daf051f432f4361606e
roobi2026 <roobineerjagan23@gmail.com>
2026-03-06 13:29:52 +0530

    Add files via upload

---
+++ Oddnum.java
@@ -0,0 +1,19 @@
+package org.testleaf.day2;
+
+public class Oddnum {
+    public static void main(String[] args) {
+        for (int i = 1; i>=1 ; i=i+2)
+        {
+            if (i>10)
+            {
+                System.out.println("End of series");
+                break;
+                
+            }
+           
+            System.out.println(i);
+
+            
+        }
+    }
+}
